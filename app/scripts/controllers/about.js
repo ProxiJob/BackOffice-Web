ab5f418582dfd5b7ab0d66a4ca3709cebbfdfbd5
Gaël Thomas <gael.thomas@outlook.fr>
2018-02-22 19:39:40 +0100

    [+-] React to Angular with Grunt serve

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name backofficeProxijobApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the backofficeProxijobApp
+ */
+angular.module('backofficeProxijobApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name backofficeProxijobApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the backofficeProxijobApp
+ */
+angular.module('backofficeProxijobApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
