ad19c17dee9e07133ff93faeb344d54857574023
Gaël Thomas <gael.thomas@outlook.fr>
2018-04-15 14:54:05 +0200

    [*] Remove dev elems - Prod test

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function createWindow() {
 		slashes: true
 	}))
 
-	mainWindow.webContents.openDevTools()
+	//mainWindow.webContents.openDevTools()
 
 	mainWindow.on('closed', function () {
 		mainWindow = null
